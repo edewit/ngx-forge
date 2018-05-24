220ea83914c35ee230c7cbcbf878054f4410473a
ia3andy <adamevin@redhat.com>
2018-05-24 15:31:01 +0200

    fix: make only one call to cluster endpoint
    - Remove the unnecessary call to cluster endpoint.
    - If there is only one connected cluster, autoselect it.

--- src/app/launcher/create-app/targetenvironment-createapp-step/target-environment-createapp-step.component.spec.ts
+++ src/app/launcher/create-app/targetenvironment-createapp-step/target-environment-createapp-step.component.spec.ts
@@ -1,27 +1,26 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { By } from '@angular/platform-browser';
-import { DomSanitizer } from '@angular/platform-browser';
-import { RouterTestingModule } from '@angular/router/testing';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { Observable } from 'rxjs';
-import { InViewportModule, WindowRef } from '@thisissoon/angular-inviewport';
+import {CommonModule} from '@angular/common';
+import {FormsModule} from '@angular/forms';
+import {RouterTestingModule} from '@angular/router/testing';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+import {Observable} from 'rxjs';
+import {InViewportModule, WindowRef} from '@thisissoon/angular-inviewport';
 
-import { LauncherComponent } from '../../launcher.component';
-import { LauncherStep } from '../../launcher-step';
-import { TargetEnvironmentCreateappStepComponent } from './target-environment-createapp-step.component';
-import { TargetEnvironment } from '../../model/target-environment.model';
-import { TargetEnvironmentService } from '../../service/target-environment.service';
-import { TokenService } from '../../service/token.service';
-import { ModalModule } from 'ngx-modal';
-import { LinkAccountsCreateappStepComponent } from '../link-accounts-createapp-step/link-accounts-createapp-step.component';
-import { MissionRuntimeService } from '../../service/mission-runtime.service';
+import {LauncherComponent} from '../../launcher.component';
+import {LauncherStep} from '../../launcher-step';
+import {TargetEnvironmentCreateappStepComponent} from './target-environment-createapp-step.component';
+import {TargetEnvironment} from '../../model/target-environment.model';
+import {TargetEnvironmentService} from '../../service/target-environment.service';
+import {TokenService} from '../../service/token.service';
+import {ModalModule} from 'ngx-modal';
+import {LinkAccountsCreateappStepComponent} from '../link-accounts-createapp-step/link-accounts-createapp-step.component';
+import {MissionRuntimeService} from '../../service/mission-runtime.service';
 
 let mockTargetEnvironmentService = {
   getTargetEnvironments(): Observable<TargetEnvironment[]> {
     let targetEnvironments = Observable.of( [{
+      /* tslint:disable */
       description: 'Here is a brief description of what OpenShift Online is. ' +
                    'There is a distinction between what OpenShift Online does compared to OpenShift.io.',
       benefits: [
@@ -35,6 +34,7 @@ let mockTargetEnvironmentService = {
       icon: 'data:image',
       id: 'os',
       styleClass: 'card-pf-footer--logo-os'
+      /* tslint:enable */
       }]);
       return targetEnvironments;
   }
