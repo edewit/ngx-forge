0d1ab3aacc510187332b5c9c418ee1326f8197a0
Corinne Krych <corinnekrych@gmail.com>
2018-08-24 13:48:06 +0200

    fix(demo): fix process.env not available in ng6

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  ANALYTICS_RECOMMENDER_URL: 'https://recommender.api.openshift.io/',
+  ANALYTICS_LICENSE_URL: 'https://license-analysis.api.openshift.io/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  ANALYTICS_RECOMMENDER_URL: 'https://recommender.api.openshift.io/',
+  ANALYTICS_LICENSE_URL: 'https://license-analysis.api.openshift.io/'
 };
 
 /*
